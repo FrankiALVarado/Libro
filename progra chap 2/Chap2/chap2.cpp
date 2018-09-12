a384b3d46e8b33e1937e77b9448d896f31f525a1
Unknown <idv16c.falvarado@uartesdigitales.edu.mx>
2018-09-11 23:57:23 -0500

    chap 2

---
+++ progra chap 2/Chap2/chap2.cpp
@@ -0,0 +1,72 @@
+#include "Includes.h"
+
+int main() // ejercicio 4
+{
+	cout << "como hacer una hamburguesa" << endl;
+	cout << "ingredientes: " << endl;
+	cout << "pan de hamburguesa" << endl;
+	cout << "carne para parrilla" << endl;
+	cout << "1 rebanada de queso amarillo" << endl;
+	cout << "rebanadas de tocino al gusto " << endl;
+	cout << "verdura fresca al gusto" << endl;
+
+	cout << "pon la carne con el queso amarillo encima a aparrillar por 20 mins, presiona enter para continuar" << endl;
+	cin.get();
+	cout << "pica la verdura que gustes y pon a freir el tocino por 15 mins, presiona enter para continuar" << endl;
+	cin.ignore();
+	cout << "prepara un plato con las mitades del pan, presiona enter para continuar" << endl;
+	cin.ignore();
+	cout << "sirve la carne y el tocino, luego hecha la verdura" << endl;
+	cout << "felicidades has cocinado una hamburguesa" << endl;
+	
+	keep_window_open();
+	return 0;
+}
+
+/*int main() ejercicio 3
+{
+	cout << "instrucciones para llegar a la universidad, de mi casa" << endl;
+	cout << "saliendo de la casa, atraviesa el porton, presiona enter para continuar" << endl;
+	cin.get();
+	cout << "gira hacia la izquierda y sigue derecho por toda avenida mexico hasta llegar a la calle andres teran, presiona enter para continuar" << endl;
+	cin.ignore();
+	cout << "sigue subiendo por andres teran hasta llegar a la glorieta de los ingenieros, presiona enter para continuar" << endl;
+	cin.ignore();
+	cout << "de la glorieta de los ingenieros atraviesa hacia el lugar donde dice Universidad de artes digitales" << endl
+		<< "felicidades, has llegado a la UAD" << endl;
+	keep_window_open();
+	return 0;
+} */
+
+/*int main() ejercicio 2
+{
+	cout << "pasos para llegar al baño, presiona enter para continuar" << endl;
+	cin.get();
+	cout << "entra a la casa, presiona enter para continuar" << endl;
+	cin.ignore();
+	cout << "sigue derecho hasta la mitad del pasillo que tienes enfrente, presiona enter para continuar" << endl;
+	cin.ignore();
+	cout << "gira a la izquierda y sube las escaleras que tienes enfrente, presiona enter para continuar" << endl;
+	cin.ignore();
+	cout << "agarra por la izquiera y abre la tercera puerta que veas, presiona enter para continuar" << endl;
+	cin.ignore();
+	cout << "felicidades has encontrado el baño" << endl;
+	
+	keep_window_open();
+	return 0;
+}*/
+
+/*int main() ejercicio 1
+{
+	cout << "hola programacion" << endl
+		<< "aqui vamos" << endl;
+	keep_window_open();
+	return 0;
+}*/
+
+/*int main() drill
+{
+	cout << "Hola mundo" << endl;
+	keep_window_open();
+	return 0;
+}*/
